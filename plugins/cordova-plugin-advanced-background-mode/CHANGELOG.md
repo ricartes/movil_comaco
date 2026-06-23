98a994db0a847f8c00dc8fd7bfa2b678900079e6
ricartes <ricardo.caartes@gmail.com>
2026-06-23 16:39:33 -0400

    chore: ignore Cordova generated outputs

--- plugins/cordova-plugin-advanced-background-mode/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# ChangeLog
-
-## 1.1.1
-- I changed my Github Username --> Links & Android Package Name adjusted
-
-## 1.1.0
-- Method `includeToTaskList` added
--- plugins/cordova-plugin-ip-mac-address/src/ios/AddressImpl.h
+++
@@ -1,9 +0,0 @@
-#import <Cordova/CDV.h>
-#import <UIKit/UIKit.h>
-
-@interface AddressImpl : CDVPlugin
-
-- (void)getIPAddress:(CDVInvokedUrlCommand*)command;
-- (void)getMACAddress:(CDVInvokedUrlCommand*)command;
-
-@end
